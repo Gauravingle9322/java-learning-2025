b95d5dee1f2ba9cd1052cf3e30876a05751d81f7
gaurav ingle <gauravingle9322@gmail.com>
2025-09-03 22:38:13 +0530

    Added Variables.java to practice Java variables

---
+++ Basics/variables.java
@@ -0,0 +1,27 @@
+class Variables {
+    public static void main(String[] args) {
+
+        // Integer variable
+        int age = 21;
+
+        // Double variable (decimal values)
+        double salary = 55000.75;
+
+        // Character variable
+        char grade = 'A';
+
+        // String variable
+        String name = "Gaurav Ingle";
+
+        // Boolean variable
+        boolean isStudent = true;
+
+        // Print all variables
+        System.out.println("---- Variable Demo ----");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student? " + isStudent);
+    }
+}
